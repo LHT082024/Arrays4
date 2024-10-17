f0d69e89755e1fb313a20ecc81f482239e8ccde5
LHT082024 <LHT082024@gmail.com>
2024-10-17 10:12:50 +0200

    made the basics of the array task

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string[] fradulentOrderIDs = new string [3];
+
+fradulentOrderIDs[0] = "A123";
+fradulentOrderIDs[1] = "B456";
+fradulentOrderIDs[2] = "C789";
+// fradulentOrderIDs[3] = "D000";
+
+Console.WriteLine($"First: {fradulentOrderIDs[0]}\nSecond: {fradulentOrderIDs[1]}\nThird: {fradulentOrderIDs[2]}");
+
+fradulentOrderIDs[0] = "D000";
+
+Console.WriteLine($"reassing value of First: {fradulentOrderIDs[0]}");
